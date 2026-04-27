3e67ed9923b1e73cb6b75e4eb6c41f4ec8da1a20
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2026-04-26 20:27:22 -0600

    [ADD] Mover printear estados a debug.h

--- source_code/main/main.c
+++ source_code/main/main.c
@@ -1,43 +1,7 @@
 #include <stdio.h>
 #include "config.h"
 #include "barcos/barco.h"
-
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include <stdlib.h>
-
-// Metodo para imprimir procesos actuales
-void print_tasks_real()
-{
-    UBaseType_t num = uxTaskGetNumberOfTasks();
-    TaskStatus_t *tasks = malloc(num * sizeof(TaskStatus_t));
-
-    uxTaskGetSystemState(tasks, num, NULL);
-
-    printf("\n===== TASK REAL STATE =====\n");
-
-    for (int i = 0; i < num; i++) {
-
-        const char *state;
-
-        switch (tasks[i].eCurrentState) {
-            case eRunning:   state = "RUNNING"; break;
-            case eReady:     state = "READY"; break;
-            case eBlocked:   state = "BLOCKED"; break;
-            case eSuspended: state = "SUSPENDED"; break;
-            case eDeleted:   state = "DELETED"; break;
-            default:         state = "UNKNOWN"; break;
-        }
-
-        printf("%-12s | %-10s | Prio: %d\n",
-               tasks[i].pcTaskName,
-               state,
-               tasks[i].uxCurrentPriority);
-    }
-
-    free(tasks);
-}
+#include "debug.h"
 
 void app_main(void)
 {
