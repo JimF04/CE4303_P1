626788e797f3de787b5dd1089b7c40d2f5e2d2e7
joscCE <j.campos@estudiantec.cr>
2026-05-03 17:37:11 -0600

    listorotongo

--- source_code/main/input/input_task.c
+++ source_code/main/input/input_task.c
@@ -17,64 +17,83 @@ extern canal_t *canal_global;
 
 
 
-#define PIN_INPUT GPIO_NUM_11
+#define PIN_INTE GPIO_NUM_11
+#define PIN_BAR GPIO_NUM_10
 
 
 void input_task(void *arg)
 {
-    // 🔧 Configurar GPIO 11 como entrada
-    gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << PIN_INPUT),
+    //Configurar GPIO 11 como entrada
+    gpio_config_t io_conf1 = {
+        .pin_bit_mask = (1ULL << PIN_INTE),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_ENABLE,
         .intr_type = GPIO_INTR_DISABLE
     };
 
-    gpio_config(&io_conf);
+    gpio_config_t io_conf2 = {
+        .pin_bit_mask = (1ULL << PIN_BAR),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
 
-    int last_level = 0;
+
+
+
+    gpio_config(&io_conf1);
+
+    gpio_config(&io_conf2);
+
+    int last_level_int = 0;
+
+    int last_level_bar = 0;
 
     while (1) {
 
-        int level = gpio_get_level(PIN_INPUT);
+        int level_int = gpio_get_level(PIN_INTE);
+        int level_bar = gpio_get_level(PIN_BAR);
 
-        // 🔥 Detectar flanco ascendente (LOW → HIGH)
-        if (level == 1 && last_level == 0) {
+
+        //Detectar flanco ascendente (LOW → HIGH)
+        if (level_bar == 1 && last_level_bar == 0) {
 
             char tipo[16];
             int direccion;
 
-            // 👉 Definí aquí qué barco querés crear
+            // Definí aquí qué barco querés crear
             strcpy(tipo, "PAT");
             direccion = 0;
 
-            printf("GPIO HIGH detectado\n");
+            printf("Otro sapo, que no haya segmentation porfa\n");
 
+            if (crear_barco(&config, tipo, direccion)) {
 
-            
-            canal_viene_buque_carepicha(canal_global);
+                int count = barcos_count();
 
+                if (count > 0) {
 
+                    barco_t *b = barcos_get(count - 1);
 
-
-
-            // if (crear_barco(&config, tipo, direccion)) {
-
-            //     int count = barcos_count();
-
-            //     if (count > 0) {
-
-            //         barco_t *b = barcos_get(count - 1);
-
-            //         if (b != NULL) {
-            //             sched.enqueue(b);
-            //         }
-            //     }
-            // }
+                    if (b != NULL) {
+                        sched.enqueue(b);
+                    }
+                }
+            }
         }
 
-        last_level = level;
+        if (level_int == 1 && last_level_int == 0) {
+
+            printf("ay viene el buque que miedo!!!!\n");
+            canal_viene_buque_carepicha(canal_global);
+
+        }
+
+
+        last_level_int = level_int;
+        last_level_bar = level_bar;
 
         vTaskDelay(pdMS_TO_TICKS(20)); // pequeño delay para estabilidad
     }
